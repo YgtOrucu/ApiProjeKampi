56eab78e3156417e1c0b296b420b339363ed8c3e
YİĞİT ÖRÜCÜ <orucuyigit@gmail.com>
2026-01-26 16:27:57 +0300

    A new Thema has been added.The UI page editing process has started.

---
+++ ApiProjeKampi.WebUI/ViewComponents/ViewComponentForUIPageLayout/HeadForUI.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.ViewComponentForUIPageLayout
+{
+    public class HeadForUI:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Views/Shared/Components/ViewComponentForUIPageLayout/HeadForUI.cshtml");
+        }
+    }
+}
---
+++ ApiProjeKampi.WebUI/ViewComponents/ViewComponentForUIPageLayout/NavbarForUI.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebUI.ViewComponents.ViewComponentForUIPageLayout
+{
+    public class NavbarForUI:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Views/Shared/Components/ViewComponentForUIPageLayout/NavbarForUI.cshtml");
+        }
+    }
+}
